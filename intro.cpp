ffdc656d31767a772aa3bdc40522e2f32f67bdad
--unset-all <--unset-all>
2020-12-12 19:33:03 +0900

    main menu design

--- intro.cpp
+++ intro.cpp
@@ -9,13 +9,32 @@
 
 using namespace std;
 
+void gotoxy(int x, int y) {
+	printf("\033[%d;%df", y, x);
+	fflush(stdout);
+}
+
 Intro::Intro() {
 	
 }
 
 
 void Intro::DrawIntro() {
-
+	system("cls");
+	gotoxy(5, 2);
+	cout << "******************************";
+	gotoxy(5, 3);
+	cout << "*        Snake Shhhh         *";
+	gotoxy(5, 4);
+	cout << "******************************";
+	gotoxy(10, 8);
+  cout << "1 PLAYER";
+	gotoxy(10, 9);
+	cout << "2 PLAYERS";
+	gotoxy(10, 10);
+	cout << "RANK" << endl;
+	gotoxy(10, 11);
+	cout << "QUIT" << endl;
 }
 
 void Intro::DrawUserCursor(int &num) {
