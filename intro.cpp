2afb2553296daa222a0c0531aa98e9f5e62c3369
--unset-all <--unset-all>
2020-12-12 19:38:12 +0900

    cxgksdjfh

--- intro.cpp
+++ intro.cpp
@@ -8,7 +8,6 @@
 #include "intro.h"
 
 using namespace std;
-
 Intro::Intro() {
 	
 }
@@ -19,9 +18,55 @@ void Intro::DrawIntro() {
 }
 
 void Intro::DrawUserCursor(int &num) {
+	if (num <= 0)
+			{
+						num = 0;
+							}
+		else if (num >= 3)
+				{
+							num = 3;
+								}
+
+			gotoxy(9, 8 + num);
+				cout << ">";
 
 }
 
 int Intro::SelectMenu() {
-	return 0;
+	int input = 0;
+		while (true)
+				{
+					DrawReadyGame();
+				  DrawUserCursor(y);
+					input = get_input();			//→←↑↓
+					if (input == MAGIC_KEY)
+						{
+							switch (get_input())
+									{
+									case North:
+										--y;
+										break;
+									case South:
+										++y;
+										break;
+									}
+						}
+
+					else if (input == SPACE)
+						{
+							switch (y)
+									{
+										case 0:
+											return 0;	//PLAYER1
+										case 1:
+											return 1;	//PLAYER2
+										case 2:
+											return 2;	//RANK
+										case 3:
+											return 3;	//QUIT
+									}
+						}
+				}
 }
+
+
